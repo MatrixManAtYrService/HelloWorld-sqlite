4d528ac27f24cabbb73063eca67ba68ed6f49e6a
unknown <mrixman@econolite.com>
2015-12-03 18:05:09 -0700

    reconfigure now blasts away build/ and recreates it.  If it's not in the source, or in CMakeLists.txt, it's now gone.

--- reconfigure.sh
+++ reconfigure.sh
@@ -4,10 +4,10 @@ if [ -f ./build/.breadcrumb ] ; then
     flag=`cat  ./build/.breadcrumb`
     if [ ! -z "$flag" ]; then
         rm -rf ./build/
-        configure.sh "$flag"
+        ./configure.sh "$flag"
     else
         echo "No breadcrumb found in ./build/"
         echo "Unable to determine how to recreate contents"
         echo "Consider running ./configure.sh first"
     fi
-
+fi
